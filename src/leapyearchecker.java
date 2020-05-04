1ca8e8bb7b416251b3fc5434be974574f6d15779
vishwa23 <vishwa.patel@treebohotels.com>
2020-05-04 15:44:12 +0530

    added testcase(year = 3200) and it fails and added condition to handle this test case

--- src/leapyearchecker.java
+++ src/leapyearchecker.java
@@ -1,4 +1,15 @@
 
 public class leapyearchecker {
+	
+	public boolean isLeapyear(int x)
+	{
+		
+			if(x%4==0)
+			{
+				return true;
+			}
+			return false;
+
+	}
 
 }
--- src/leapyearcheckertest.java
+++ src/leapyearcheckertest.java
@@ -4,10 +4,30 @@ import org.junit.jupiter.api.Test;
 
 class leapyearcheckertest {
 
+//	creating test object
+	leapyearchecker testObject;
+	
+//	set testObject to new
+//	void setUp() {
+//		
+//		testObject = new leapyearchecker();
+//	}
+	
+//	checking 
 	@Test
 	void test() throws Exception {
-		leapyearchecker testObject = new leapyearchecker();
+		leapyearchecker testObject = new leapyearchecker(); 
 		assertNotNull(testObject);
+	}
+	
+	
+//	checking first TestCase(year = 3200) for leapyear and if test fails then add conditions in the isLeapyear() function
+	@Test
+	void firsttest() throws Exception{
+		
+		leapyearchecker testObject = new leapyearchecker();
+		
+		assertTrue(testObject.isLeapyear(3200));
 	}
 
 }
