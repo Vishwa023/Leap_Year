0e3d114f9c1397cce25bd774e8f142f9de89d7cd
vishwa23 <vishwa.patel@treebohotels.com>
2020-05-04 15:09:19 +0530

    creating test_case file

---
+++ src/leapyearcheckertest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class leapyearcheckertest {
+
+	@Test
+	void test() throws Exception {
+		leapyearchecker testObject = new leapyearchecker();
+		assertNotNull(testObject);
+	}
+
+}
